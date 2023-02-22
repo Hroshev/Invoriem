4c3501986845b5db033f19d44b4a78e87fb9fc4f
unknown <grosheff.ivan@gmail.com>
2023-02-22 17:53:30 +0200

    finish work

--- src/js/modules/date.js
+++ src/js/modules/date.js
@@ -1,4 +1,8 @@
 const date = new Date();
 const year = date.getFullYear();
 
-document.getElementById('copyright').innerHTML = `<p>Copyright © ${year} Invoriem</p>`;
+const copyright = document.querySelectorAll('.copyright');
+
+for (let i = 0; i < copyright.length; i++) {
+    copyright[i].innerHTML = `<p>Copyright © ${year} Invoriem</p>`;
+}
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,26 +1,20 @@
 import Swiper from "swiper/bundle";
 
 const swiper = new Swiper(".slider", {
-  slidesPerView: 3,
-  spaceBetween: 48,
-  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+autoplay: {
+  delay: 2500,
+  disableOnInteraction: false,
+},
+breakpoints: {
+  830: {
+    slidesPerView: 2,
+    spaceBetween: 30,
+  },
+  1770: {
+    slidesPerView: 3,
+    spaceBetween: 48,
+  },
+},
 });
-
-
-
-// slidesPerView: 3,
-// spaceBetween: 48,
-// autoplay: {
-//   delay: 2500,
-//   disableOnInteraction: false,
-// },
-// breakpoints: {
-//   576: {
-//     slidesPerView: 2,
-//     spaceBetween: 30,
-//   },
-//   992: {
-//     slidesPerView: 4,
-//     spaceBetween: 30,
-//   },
-// },
